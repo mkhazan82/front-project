9610d9866a2fa60ff643bff0dcb345c9b1bfbf36
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-11-29 12:47:27 +0330

    Update addProduct.php

--- addProduct.php
+++ addProduct.php
@@ -1,24 +1,17 @@
 <?php
 
-$link=new mysqli('localhost','root','','register');
-$link ->set_charset("utf8");
+include 'settings.php';
+include 'db.php';
+$db = new db($dbHost, $dbUser, $dbPass, $dbName);
 
-if($link-> connect_errno){
-    echo "مشکلی در اتصال به پایگاه داده به وجود آمده".$link-> connect_error;
-    exit();
-}
 $sql="INSERT INTO products (p_name,p_material,p_count,body_text)
-VALUES ('{$_POST['p_name']}','{$_POST['p_material']}','{$_POST['p_count']}',
-'{$_POST['body_text']}')";
+VALUES (?,?,?,?,?,?,?,?,?)";
 
-if($result=$link->query($sql))
-{
+$result=$db->query($sql,$_POST['p_name'],$_POST['p_material'],$_POST['p_count'],
+$_POST['body_text']);
+
     echo"محصول با موفقیت افزوده شد";
-    $link->close();
-}
-
-
-
+    $db->close();
 
 
 ?>
