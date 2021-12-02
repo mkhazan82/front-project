8c0f7fcb139277c2e82a9cac1a6b245db7c5bdd3
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-12-02 11:54:35 -0800

    Update editProduct.php

--- editProduct.php
+++ editProduct.php
@@ -8,20 +8,22 @@
     <body>
   
         <form action=""method="post"enctype="multipart/form-data">
-            <?php var_dump($product_color); ?>
+           
         
             
          <label for="p_name">نام محصول :</label>  
          <input type="text"name="p_name"id="p_name"class="text text-fulname"value="<?php  echo $product_name  ?>">
          
          <br>
-         <label for="p_material">جنس محصول :</label>  
-         <input type="text"name="p_material"id="p_material"class="text text-fulname"value="<?php  echo $product_material  ?>">
+         <label for="p_date">قیمت  :</label>  
+         <input type="text"name="price"id="price"class="text text-fulname"value="<?php  echo $product_price  ?>">
          <label for="p_count" class="short-text-label">تعداد :</label>
          <input type="number"name="p_count"id="p_count"class="text short-text"value="<?php  echo $product_count ?>">
-
-         <br> <br>
-     
+         <label for="p_image">تصویر محصول:</label>  
+         <input type="text"name="image"id="image"class="text text-fulname"value="<?php  echo $product_image ?>">
+      
+         <br>
+             
       <label for="body-text">توضیحات :</label>
         
       <textarea name="body_text"id="body_text"class="text multi-text"><?php  echo $product_detail  ?></textarea>
