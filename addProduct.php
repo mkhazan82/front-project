16616380342e27b521b78365b597cf9b47fc3de1
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-12-02 11:50:47 -0800

    Update addProduct.php

--- addProduct.php
+++ addProduct.php
@@ -4,10 +4,10 @@ include 'settings.php';
 include 'db.php';
 $db = new db($dbHost, $dbUser, $dbPass, $dbName);
 
-$sql="INSERT INTO products (p_name,p_material,p_count,body_text)
-VALUES (?,?,?,?,?,?,?,?,?)";
+$sql="INSERT INTO products (p_name,price,p_count,p_image,p_width,p_height,p_valency,p_deep,color,body_text)
+VALUES (?,?,?,?,?,?,?,?,?,?)";
 
-$result=$db->query($sql,$_POST['p_name'],$_POST['p_material'],$_POST['p_count'],
+$result=$db->query($sql,$_POST['p_name'],$_POST['price'],$_POST['p_count'],$_POST['p_image'],
 $_POST['body_text']);
 
     echo"محصول با موفقیت افزوده شد";
