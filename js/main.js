16956c37bea4265776607a4b54dc78c6ad305c9c
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-12-15 07:12:38 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,58 +1,28 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed:400,
-    spaceBetween: 7, 
-   
-   
-  autoplay:true,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed:600,
+     
+autoplay:true,
 
-   spaceBetween:8,
-   slidesPerView:1,
-   spaceBetween: 8 ,
+ slidesPerView:1,
+ spaceBetween:0,
+    
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    
+  },
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-  
-  const swiper_grid = new Swiper('.grid-swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-   
-    autoplay:true,
-    spaceBetween:8,
-   slidesPerView:3,
-  
-
-
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-      
-  });
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
