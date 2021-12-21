4a091c7ca861847a380c183f3a16a75790ed2983
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-12-21 09:22:46 -0800

    Update and rename addProduct.php to addProductControler.php

--- addProductControler.php
+++ addProductControler.php
@@ -7,8 +7,7 @@ $db = new db($dbHost, $dbUser, $dbPass, $dbName);
 $sql="INSERT INTO products (p_name,price,p_count,p_image,body_text)
 VALUES (?,?,?,?,?,?,?,?,?,?)";
 
-$result=$db->query($sql,$_POST['p_name'],$_POST['price'],$_POST['p_count'],$_POST['p_image'],
-$_POST['body_text']);
+$result=$db->query($sql,$_POST['p_name'],$_POST['price'],$_POST['p_count'],$_POST['p_image'],$_POST['body_text']);
 
     echo"محصول با موفقیت افزوده شد";
     $db->close();
