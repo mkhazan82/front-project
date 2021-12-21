aba529a0508ccc33d7405dab4a02d4990a72b325
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-12-21 09:09:34 -0800

    Update admin-listProduct-view.php

--- admin-listProduct-view.php
+++ admin-listProduct-view.php
@@ -1,13 +1,7 @@
-<!doctype html>
-<html lang="fa">
-    <head>
-        <title> مدیریت محصولات</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="css/style.css" type="text/css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+<?php
 
-    </head>
-    <body>
+include 'adminPanel-view.php';
+?>
        
 
    <table class="datatable"> 
@@ -15,7 +9,6 @@
 <th>نام کالا</th>
 <th>قیمت (تومان)</th>
 <th>تعداد</th>
-<th>توضیحات</th>
 <th>تصویر بندانگشتی</th>
 <th></th>
 <th></th>
@@ -29,12 +22,12 @@ foreach($productList as $product)
 <td>{$product['p_name']}</td>
 <td>{$product['price']}</td>
 <td>{$product['p_count']}</td>
-<td>{$product['body_text']}</td>
+
 <td><img src='{$product['p_image']}'class='img-tumbnail'></td>
 <td><a href ='editProduct-controler.php?id={$product['id']}'class='btn btn-outlined-button '>ویرایش</a></td>
 <td><a href ='admin-deleteProduct-controler.php?id={$product['id']}'class='btn btn-outlined-button'>حذف</a></td>
 </tr>
-   
+  
    ";
 }
 
@@ -42,10 +35,11 @@ foreach($productList as $product)
       </table>
       <br>
         
-        <a href="addProduct.html" class="btn btn-filled primary ">افزودن محصول
+        <a href="addProduct-view.php" class="btn btn-filled primary ">افزودن محصول
         <i class="fas fa-plus-circle"></i>
         </a>
 
 <br>
+</section>
     </body>
 </html>
