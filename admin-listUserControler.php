77a7d2a07f3b7cc85f0df6d56c1e8048d260a62f
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-12-27 07:15:05 -0800

    Update admin-listUserControler.php

--- admin-listUserControler.php
+++ admin-listUserControler.php
@@ -2,14 +2,19 @@
 include 'settings.php';
 include 'db.php';
 
+
+
 $db = new db($dbHost, $dbUser, $dbPass, $dbName);
+
 
 $sql="SELECT* FROM users  LIMIT 10";
 
 $result = $db ->query($sql);
 $userList=$result ->fetchAll();
 
+   
 include 'admin-listUser-view.php';
+
 
 $db->close();
 ?>
