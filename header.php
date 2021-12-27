e8ae95f94a3ee4305531b59ab27df4ca9d0b8dc0
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2021-12-27 07:18:29 -0800

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,24 @@
+<?php
+session_start();
+
+
+?>
 <!doctype html>
 <html lang="fa">
     <head>
-        <title> لی لی</title>
+    <title> لی لی</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
         <link  rel="stylesheet"   href="https://unpkg.com/swiper@7/swiper-bundle.min.css" type="text/css"/>
     </head>
     <body class="grid-layout">
     
     <header> 
-      <h1>فروشگاه شال و روسری</h1>
+    <h1>فروشگاه شال و روسری</h1>
          <nav>
+         <a href="register-view.php"id="registerlink"class="btn btn-filled primary">ثبت نام</a>
+   <a href="loginControler.php"id="loginlink"class="btn btn-outlined-button"> ورود </a>
      <ol id="mainNav">
    
    <li><a href="home-controler.php"class="">خانه </a></li>
@@ -19,9 +27,16 @@
    <li><a href="#"class="">درباره ما </a></li>
   
    <li><a href="contact-view.php"class="">تماس باها</a></li>
-   <li><a href="register-view.php"id="registerlink"class="">ثبت نام</a></li>
-   <li><a href="login.html"id="loginlink"class=""> ورود </a></li>
+  
 </ol>
      </nav>
+
      </header>
+    <?php
+     
+  
+      include 'user_panel.php';
+    
+    ?> 
+    
     <section>
