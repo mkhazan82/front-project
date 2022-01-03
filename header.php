1da33a7468339a3959158f5bf0b72bb9855d466a
mkhazan82 <92431214+mkhazan82@users.noreply.github.com>
2022-01-03 23:55:50 +0330

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="fa">
     <head>
-        <title>  نوین</title>
+        <title>  لی لی</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css" type="text/css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
